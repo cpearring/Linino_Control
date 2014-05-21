c6678ec503561b8c1190d59f69dcc4eb280d85c2
Richard Kerr <rmkerr@hawaii.edu>
2014-05-21 11:37:16 -1000

    Major functionality added. Sends and recieves RPM. Still isn't dealing with char string unpus well.

--- main_control.py
+++ main_control.py
@@ -9,7 +9,7 @@ from struct import *
 sys.path.insert(0, '/usr/lib/python2.7/bridge/') 
 from bridgeclient import BridgeClient as bridgeclient
 from tcp import TCPJSONClient
-json = TCPJSONClient('127.0.0.1', 5700)
+json = TCPJSONClient('', 5700)
 
 class mysocket:
     '''
@@ -20,7 +20,7 @@ class mysocket:
         self.UDP_IP = host
         self.UDP_PORT = port  
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(("", port))
+        self.sock.bind(("192.168.240.1", port))
         self.sock.setblocking(0)
     def mysend(self, msg):
         #self.sock.sendto(bytes(msg), (self.UDP_IP, self.UDP_PORT)) Probably th right line
@@ -29,7 +29,7 @@ class mysocket:
     def myreceive(self):
         result = select.select([self.sock],[],[],0)
         if(result[0] != []):
-            return result[0][0].recv(128) 
+            return result[0][0].recv(4) 
         return None
     def end(self):
         self.sock.close()
@@ -38,24 +38,43 @@ class RobotStatus:
     LeftRPM = 0
     RightRPM = 0
     BrakeStatus = 0
+    Value = 0
     def updateStatus(self):
-        value = bridgeclient()
-        try:
-            #L = value.get('LEFT_RPM')
-            x = json.loads({'command':'get', 'key':'LEFT_RPM' })
-            for i in x:
-                print i
-            if L is not None:
-                if ( len(L) == 0 ):
-                    self.LeftRPM = 0
-                elif ( len(L) == 1 ):
-                    self.LeftRPM = (ord(L[0]))#+ ((ord(L[1])-1)<<8)
-                else:
-                    self.LeftRPM = (ord(L[0])) + ((ord(L[1]))<<8)
-                print self.LeftRPM
-        except:
-            print None
+        L = self.Value.get('RPM_STATUS')
+        if L is not None:
+            self.LeftRPM = (ord(L[0])-1) + ((ord(L[1])-1)<<8)
+            self.RightRPM = (ord(L[2])-1) + ((ord(L[3])-1)<<8)
+            if self.LeftRPM >= 32511:
+                self.LeftRPM = -(self.LeftRPM%32511)
+            if self.RightRPM >= 32511:
+                self.RightRPM = -(self.RightRPM%32511)
+            print self.LeftRPM
+            print self.RightRPM
+	'''
+        #print "Entering updateStatus()"
+        L = self.Value.get('LEFT_RPM')
+        #print "Got value from Value.get()"
+	#Left side--------------------------------------------------------
+        if L is not None:
+            if ( len(L) == 0 ):
+                self.LeftRPM = 0
+            elif ( len(L) == 1 ):
+                self.LeftRPM = (ord(L[0]))#+ ((ord(L[1])-1)<<8)
+            else:
+                self.LeftRPM = (ord(L[0])) + ((ord(L[1]))<<8)
+            print self.LeftRPM
+        #Right Side-------------------------------------------------------
+        L = self.Value.get('RIGHT_RPM')
+        if L is not None:
+            if ( len(L) == 0 ):
+                self.LeftRPM = 0
+            elif ( len(L) == 1 ):
+                self.LeftRPM = (ord(L[0]))
+            else:
+                self.LeftRPM = (ord(L[0])) + ((ord(L[1]))<<8)
+            print self.LeftRPM 
         #self.RightRPM = ord(L[2])+ (ord(L[3])<<8)
+        #print "Exiting updateStatus()"'''
     def generatePacket(self):
         '''s = []
         s.append(self.RightRPM%256)
@@ -65,9 +84,9 @@ class RobotStatus:
         s.append(self.LeftRPM/256)
 
         s.append(self.BrakeStatus)'''
-
+        #print "Packing values..."
         return pack('<hhb', self.RightRPM, self.LeftRPM, self.BrakeStatus)
-
+        #print "Finished packing"
         #return ''.join([bin(item) for item in s])
 
 s_test = mysocket()
@@ -76,21 +95,36 @@ s_test.connect('192.168.240.155', 30001)
 
 
 #robot.LeftRPM = 123
-
+robot = RobotStatus()
+robot.Value = bridgeclient()
 while(1):
-    robot = RobotStatus()
-    i = s_test.myreceive()
-    robot.updateStatus()
+    start = time.time() 
+    
     s_test.mysend(robot.generatePacket())
+    i = s_test.myreceive()
     if( i != None):
         #command recived. Process it----------------------------
         b = unpack('<bbb', i)
-        print b
         if( b[0] == 1):
-            t = bridgeclient()
-            r = [b[1],b[2]]
-            #t.put('SET_L_RPM', r)
+            #r = [str(unichr(b[1])),str(unichr(b[2]))]
+            if b[1] < 0:
+                sg_1 = -b[1] + 128
+            else:
+                sg_1 = b[1]
+            if b[2] < 0:
+                sg_2 = -b[2] + 128
+            else:
+                sg_2 = b[2]
+            print sg_1
+            print sg_2
+            r = [chr(sg_1),chr(sg_2)]
+            print r
             json.send({'command':'put', 'key':'SET_L_RPM', 'value':r })
+    #print "Hanging in json.send?"
     #if CAN message recieved, update our model and then send out new info to control station   
+    robot.updateStatus()
+    end = time.time()
+    print "Elapsed time:"
+    print end - start
 
 s_test.end()
